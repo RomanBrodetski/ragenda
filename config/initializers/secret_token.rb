552b0cac1787f236f6828fec05faf7f47931f8b2
unknown <roman.brodetski@gmail.com>
2013-05-21 15:43:21 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ragenda::Application.config.secret_token = '6de01080de19b706eff78a6cff162de1d028928146889d0cb724a9e25870f1a89b41b3776a20a5ffa337b0faf9896d57356045ae7f0389946934ff58adb315fe'
